2a262b07247e064374bf7cf5ecb6cf7d1713e81a
DayvsonDvs <dayvson_dvs@hotmail.com>
2022-10-13 15:57:13 -0300

    Fix: Sum function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-const languages = require('./programming-languages.json')
-const sum = function(n1, n2){
+export default sum = function(n1, n2){
   return n1+n2
 }
 
-module.exports = { all:languages, sum}
