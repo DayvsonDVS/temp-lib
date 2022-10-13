91353a7986ced6d3d32aadefa766089890ebe1d9
DayvsonDvs <dayvson_dvs@hotmail.com>
2022-10-13 15:40:07 -0300

    Feat: Sum function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 const languages = require('./programming-languages.json')
+const sum = function(n1, n2){
+  return n1+n2
+}
 
-module.exports = { all:languages}
+module.exports = { all:languages, sum}
