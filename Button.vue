0c19224388892dae939a4f86865ac6ae29a1be07
DayvsonDvs <dayvson_dvs@hotmail.com>
2022-10-13 20:56:56 -0300

    refact: Calculator

--- Button.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="logo">
-    <button>teste</button>
-  </div>
-</template>
--- Calculator.ts
+++ Calculator.ts
@@ -1,14 +1,6 @@
-import Button from './Button'
-
-export default {
-  components: {
-    Button
-  }
+export function Sum(n1: number, n2: number) {
+  return n1 + n2
 }
-
-// export function Sum(n1: number, n2: number) {
-//   return n1 + n2
-// }
-// export function Sub(n1: number, n2: number) {
-//   return n1 - n2
-// }
+export function Sub(n1: number, n2: number) {
+  return n1 - n2
+}
