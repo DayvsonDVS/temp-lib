00d2e6153bcaa5fd4406e678dcf1092d077e2255
DayvsonDvs <dayvson_dvs@hotmail.com>
2022-10-13 19:38:36 -0300

    Feat: teste component

--- Calculator.ts
+++ Calculator.ts
@@ -1,3 +1,7 @@
+import Logo from './components/atoms/Logo.vue'
+
+export default Logo
+
 export function Sum(n1: number, n2: number) {
   return n1 + n2
 }
---
+++ components/atoms/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="logo">
+    <img @click="navigateTo('/')" src="/images/logo/logo.svg" alt="Darpi" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.logo {
+  width: 100%;
+  cursor: pointer;
+}
+</style>
